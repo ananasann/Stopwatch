2cc40503acd87ff1df318482ebdd440cb940ae00
Ananasann <paramonovaann39@gmail.com>
2021-09-21 15:04:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.kt
@@ -0,0 +1,54 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_stopwtch.*
+import java.util.*
+
+class StopwatchActivity : AppCompatActivity() {
+
+    private var seconds: Int = 0
+    private var running = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stopwtch)
+        runTimer()
+    }
+
+    private fun runTimer() {
+        val handler: Handler = Handler()
+        handler.post(
+            object: Runnable {
+                override fun run() {
+                    val hours = seconds / 3600
+                    val minutes = (seconds % 3600) / 60
+                    val secs = seconds % 60
+                    val time = String.format(
+                        Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs
+                    )
+                    time_view.text = time
+                    if (running) {
+                        seconds++
+                    }
+                    handler.postDelayed(this,1000)
+                }
+            })
+    }
+
+    fun onClickStart(view: View) {
+        running = true
+    }
+
+    fun onClickStop(view: View) {
+        running = false
+    }
+
+    fun onClickReset(view: View) {
+        running = false
+        seconds = 0
+    }
+
+}
